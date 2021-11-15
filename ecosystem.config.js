486535812c9d1cd11cbf37ef9b1df2d698341145
noook <neil.richter@golem.ai>
2021-11-15 17:06:22 +0100

    build: add pm2 command

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Portfolio',
+      exec_mode: 'cluster',
+      instances: 2,
+      script: '.app/node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
